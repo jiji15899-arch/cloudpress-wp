dc494da54cfb29188f335f17985a5f8809ef5c86
jiji15899-arch <jiji15899@gmail.com>
2026-04-18 13:24:52 +0900

    Update index.js

--- functions/api/user/index.js
+++ functions/api/user/index.js
@@ -1,5 +1,5 @@
 // functions/api/user/index.js
 // 사용자 프로필 + CF Global API 키 + 2FA 관리
 
-import { CORS, _j, ok, err, handleOptions, getToken, getUserFull as getUser, hashPw } from './_shared.js';
+import { CORS, _j, ok, err, handleOptions, getToken, getUserFull as getUser, hashPw } from '../_shared.js';
 
